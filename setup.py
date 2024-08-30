ebedabcdf3e27be5edf948bd3f94b1b73adad708
SergeyZinchenko <zinchse@MacBook-Air.local>
2024-08-30 17:10:49 +0700

    fix black issue in setup

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ def parse_requirements(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
+
 setup(
     name="hbo_bench",
     version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    include_package_data=True,  # Включает данные пакета
-    package_data={"hbo_bench": ["py.typed"]},  # Включает py.typed файл
+    include_package_data=True,
+    package_data={"hbo_bench": ["py.typed"]},
     install_requires=parse_requirements("requirements.txt"),
 )
