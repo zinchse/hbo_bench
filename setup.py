b04b592dd7682d674052ad81db236403ee093f2e
SergeyZinchenko <zinchse@MacBook-Air.local>
2024-08-30 16:31:30 +0700

    fix file structure issues

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from setuptools import setup, find_packages
 
 
-# pragma: no cover
 def parse_requirements(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
-
 setup(
     name="hbo_bench",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=parse_requirements("requirements.txt"),
 )
