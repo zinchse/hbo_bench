d3ab7e81575a579438efabdab260130ec0b00e91
SergeyZinchenko <zinchse@MacBook-Air.local>
2024-08-30 15:51:03 +0700

    fix problem with import

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 
+# pragma: no cover
 def parse_requirements(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
