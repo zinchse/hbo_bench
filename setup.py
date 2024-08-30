a44e27e22979b2d522dfe24d396c962290a9811c
SergeyZinchenko <zinchse@MacBook-Air.local>
2024-08-30 13:09:14 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+setup(
+    name='hbo_bench',
+    version='0.1',
+    packages=find_packages(),  
+    install_requires=parse_requirements('requirements.txt'),
+)
